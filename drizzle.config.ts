1b1afdba5e975378f3f700d7f87c8059bd93034d
{Nycolas Santos} <{nycolasbrsantos@gmail.com}>
2025-07-12 19:23:56 -0300

    refactor: update drizzle configuration and dependencies for improved setup
    - Simplified drizzle.config.ts by removing dotenv configuration and directly importing dotenv.
    - Updated package.json and package-lock.json to downgrade drizzle-orm and pg versions for compatibility.
    - Refactored database connection setup in src/db/index.ts to streamline the initialization process.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,18 +1,11 @@
-    import { defineConfig } from "drizzle-kit";
-    import * as dotenv from "dotenv";
+import 'dotenv/config';
+import { defineConfig } from 'drizzle-kit';
 
-    dotenv.config({
-      path: ".env.local",
-    });
-
-    export default defineConfig({
-      schema: "./src/db/schema.ts",
-      out: "./drizzle",
-      dialect: "postgresql",
-      dbCredentials: {
-        url: process.env.DATABASE_URL!,
-      },
-      verbose: true,
-      strict: true,
-    });
-    
+export default defineConfig({
+  out: './drizzle',
+  schema: './src/db/schema.ts',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,17 +1,4 @@
-import { drizzle } from 'drizzle-orm/postgres-js'
-import postgres from 'postgres'
-import * as dotenv from 'dotenv'
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/node-postgres';
 
-dotenv.config({
-  path: '.env.local',
-})
-
-if (!process.env.DATABASE_URL) {
-  throw new Error('DATABASE_URL is not set in .env.local')
-}
-
-// Cliente de conexão do Postgres
-const client = postgres(process.env.DATABASE_URL)
-
-// Instância do Drizzle que usaremos em toda a aplicação
-export const db = drizzle(client)
+export const db = drizzle(process.env.DATABASE_URL!);
